2d8a53df918a3f2e44c93ca7a42f4aab4061ad58
caoyu <1127caoyu@gmail.com>
2021-03-12 10:32:09 +0800

    签到成功界面

--- pages/checkin_result/checkin_result.vue
+++ pages/checkin_result/checkin_result.vue
@@ -12,29 +12,58 @@
 		</view>
 
 		<view class="result-container">
-			<view class="left">
-				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
-				<view class="line"></view>
-				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
-			</view>
-			<view class="right">
-				<view class="start">
-					上班时间（时间 {{}})
-				</view>
-				<view class="checkin">签到时间（{{}})</view>
-				<view class="address">
-					<image class="icon-small" mode="widthFix" src="../../static/icon-7.png"></image>
-					<text></text>
-					<text></text>
-
-				</view>
-				<view class="face">
-					<image class="icon-small" mode="widthFix" src="../../static/icon-8.png"></image>
-					<text></text>
-					<text></text>
-				</view>
-				<view class="end">下班时间（时间 {{}})</view>
-			</view>
+		    <view class="left">
+		        <image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+		        <image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+		        <view class="line"></view>
+		    </view>
+		    <view class="right">
+		        <view class="row">
+		            <text class="start">上班（时间 {{ attendanceTime }}）</text>
+		        </view>
+		        <view class="row">
+		            <text class="checkin-time">签到时间（{{ checkinTime }}）</text>
+		            <text class="checkin-result other green" v-if="status == '正常'">{{ status }}</text>
+		            <text class="checkin-result other yellow" v-if="status == '迟到'">{{ status }}</text>
+		        </view>
+		        <view class="row">
+		            <image src="../../static/icon-7.png" mode="widthFix" class="icon-small"></image>
+		            <text class="desc">{{ address }}</text>
+		            <text class="checkin-result green" v-if="risk == '低风险'">{{ risk }}</text>
+		            <text class="checkin-result yellow" v-if="risk == '中风险'">{{ risk }}</text>
+		            <text class="checkin-result red" v-if="risk == '高风险'">{{ risk }}</text>
+		        </view>
+		        <view class="row">
+		            <image src="../../static/icon-8.png" mode="widthFix" class="icon-small"></image>
+		            <text class="desc">身份验证</text>
+		            <text class="checkin-result green">已通过</text>
+		        </view>
+		        <view class="row">
+		            <text class="end">下班（时间 {{ closingTime }}）</text>
+		        </view>
+		    </view>
+		</view>
+		
+		<view class="checkin-report">
+		    <image src="../../static/big-icon-1.png" mode="widthFix" class="big-icon"></image>
+		    <view class="report-title">
+		        <text class="days">{{ checkinDays }}</text>
+		        <text class="unit">天</text>
+		    </view>
+		    <view class="sub-title">
+		        <text>累计签到</text>
+		        <view class="line"></view>
+		    </view>
+		    <view class="calendar-container">
+		        <view class="calendar" v-for="one in weekCheckin" :key="one">
+		            <image src="../../static/icon-9.png" mode="widthFix" class="calendar-icon" v-if="one.type != '节假日'"></image>
+		            <image src="../../static/icon-10.png" mode="widthFix" class="calendar-icon" v-if="one.type == '节假日'"></image>
+		            <text class="day">{{ one.day }}</text>
+		            <text class="result green" v-if="one.status == '正常'">正常</text>
+		            <text class="result yellow" v-if="one.status == '迟到'">迟到</text>
+		            <text class="result red" v-if="one.status == '缺勤'">缺勤</text>
+		        </view>
+		    </view>
 		</view>
 
 	</view>
